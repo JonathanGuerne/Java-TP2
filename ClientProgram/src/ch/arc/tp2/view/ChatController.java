9c09103a8082b41630848b9f4785779268486a07
Fleury Anthony <anth.fleury@gmail.com>
2017-06-04 12:02:18 +0200

    UI Chat + Rapport

--- ClientProgram/src/ch/arc/tp2/view/ChatController.java
+++ ClientProgram/src/ch/arc/tp2/view/ChatController.java
@@ -13,6 +13,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.stage.FileChooser;
 
 import java.awt.*;
